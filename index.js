4424fba27886b0203f8537d7bdebe61dcb6f43de
Catfish21701 <katrilantto@gmail.com>
2019-01-12 19:15:05 +0100

    Bot token now fetched at start of script

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const config = require('./config.json');
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const token = (process.env.token ? process.env.token : config.token);
 
 client.on('ready', () => {
     console.log('Ready!');
@@ -12,4 +13,4 @@ client.on('message', message => {
     }
 });
 
-client.login(process.env.token ? process.env.token : config.token);
+client.login(token);
